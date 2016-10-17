f2de266b5d3f91f2c254e569ca722bb2d7941da8
Mafooch <joshuamaffuccio@gmail.com>
2016-10-16 21:32:22 -0400

    Get working view model functionality

--- app.js
+++ app.js
@@ -1,25 +1,64 @@
 (function() {
+
 "use strict";
 
 angular.module("NarrowItDownApp", [])
 .controller("NarrowItDownController", NarrowItDownController)
-.provider("MenuSearchService", MenuSearchService);
-
+.service("MenuSearchService", MenuSearchService)
+.constant("ApiBasePath", "https://davids-restaurant.herokuapp.com");
+// .directive("FoundItemsDirective", FoundItemsDirective);
 
 NarrowItDownController.$inject = ["MenuSearchService"];
-function NarrowItDownController() {
+function NarrowItDownController(MenuSearchService) {
+  var search = this;
 
+  search.searchTerm = "";
+
+  search.showMatchedItems = function() {
+    var promise = MenuSearchService.getMatchedMenuItems(search.searchTerm);
+
+    promise.then(function (response) {
+      search.found = response;
+      if(response.length === 0) {
+        search.nothingFound = true;
+      } else {
+        search.nothingFound = false;
+      }
+    });
+  };
 }
 
-function MenuSearchService() {
+
+MenuSearchService.$inject = ["$http", "ApiBasePath"];
+function MenuSearchService($http, ApiBasePath) {
   var service = this;
 
   service.getMatchedMenuItems = function(searchTerm) {
-    return $http(..).then(function(result) {
-      // process result and only keep items that match
+    return $http({method: "GET", url: (ApiBasePath + "/menu_items.json")}).then(function(result) {
+      var menuItems = result.data.menu_items;
       var foundItems = [];
-
+      for(var i = 0; i < menuItems.length; i++) {
+        if(searchTerm.length === 0) {
+          break;
+        }
+        var description = menuItems[i].description;
+        if(description.toLowerCase().indexOf(searchTerm) !== -1) {
+          foundItems.push(menuItems[i]);
+        }
+      }
       return foundItems;
     });
   };
 }
+
+function FoundItemsDirective() {
+  // var ddo = {
+  //   scope: {
+  //     found: "<"
+  //   }
+  // };
+  //
+  // return ddo;
+}
+
+})();
